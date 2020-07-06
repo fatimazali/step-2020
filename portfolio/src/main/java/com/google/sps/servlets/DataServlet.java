876a1cf0722d5bde34f154fe117e4207afc0f98c
Fatima <alifatima168@gmail.com>
2020-07-06 19:17:47 +0000

    Add locations to fatimaLocations

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -19,10 +19,16 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
 
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
+
+  ArrayList<String> fatimaLocations = new ArrayList<String>();
+  fatimaLocations.add("Lansing, Michigan");
+  fatimaLocations.add("Scott's Valley, California");
+  fatimaLocations.add("Cupertino, California");
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
