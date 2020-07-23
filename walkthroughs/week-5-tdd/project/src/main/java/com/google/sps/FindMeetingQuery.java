79b62f59dc10c6ed448ff79b47e53ebfd3e1ac48
Fatima <alifatima168@gmail.com>
2020-07-23 09:22:00 +0000

    Define helper methods

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -15,9 +15,56 @@
 package com.google.sps;
 
 import java.util.Collection;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Comparator;
+import java.util.Collections;
 
 public final class FindMeetingQuery {
+  /**
+   * Return time ranges when an event can be scheduled for all mandatory attendes of the meeting request.
+   *
+   * @param title The human-readable name for the event. Must be non-null.
+   * @param when The time when the event takes place. Must be non-null.
+   * @param attendees The collection of people attending the event. Must be non-null.
+   */
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
     throw new UnsupportedOperationException("TODO: Implement this method.");
   }
+
+  /**
+   * Find all events that will be attended by any mandatory attendee of the request.
+   *
+   * @param title The human-readable name for the event. Must be non-null.
+   * @param when The time when the event takes place. Must be non-null.
+   * @param attendees The collection of people attending the event. Must be non-null.
+   */
+   private Collection<TimeRange> findEvents(Collection<Event> events, MeetingRequest request, QueryType type) {
+
+   }
+
+  /**
+   * Determine the overlap between all event times. 
+   *
+   * @param title The human-readable name for the event. Must be non-null.
+   * @param when The time when the event takes place. Must be non-null.
+   * @param attendees The collection of people attending the event. Must be non-null.
+   */
+   private Collection<TimeRange> determineUnavailableTimes(Collection<Event> events, MeetingRequest request, QueryType type) {
+
+   }
+
+  /**
+   * Find the time in each day outside of the range of unavailable times.
+   *
+   * @param title The human-readable name for the event. Must be non-null.
+   * @param when The time when the event takes place. Must be non-null.
+   * @param attendees The collection of people attending the event. Must be non-null.
+   */
+   private Collection<TimeRange> determineAvailableTimes(Collection<Event> events, MeetingRequest request, QueryType type) {
+
+   }
+      
 }
